182c9d01d31af24708ed852e6861204fb5034c55
Rodrigo Carranza <rodricarranza@gmail.com>
2014-09-17 00:46:55 -0300

    .

--- capitulo4/auto/capt4.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook "capt4"
- (lambda ()
-    (TeX-run-style-hooks
-     "capitulo4/implementacion"
-     "capitulo4/diseno"
-     "capitulo4/resultados"
-     "capitulo4/analisis")))
-
